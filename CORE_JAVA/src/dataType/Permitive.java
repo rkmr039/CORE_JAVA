fc2ae0cf51d0c46f0e35da177b510a9920febc29
RISHAB <rkmr039@gmail.com>
2019-06-12 13:00:23 +0530

    Primitive Data type Range

---
+++ CORE_JAVA/src/dataType/Permitive.java
@@ -0,0 +1,17 @@
+package dataType;
+
+public class Permitive {
+
+	public static void main(String[] args) {
+		// byte short int long
+		// float double
+		// char
+		// boolean
+		System.out.println("byte   "+Byte.MIN_VALUE+   " "+Byte.MAX_VALUE);
+		System.out.println("short  "+Short.MIN_VALUE+  " "+Short.MAX_VALUE);
+		System.out.println("int    "+Integer.MIN_VALUE+" "+Integer.MAX_VALUE);
+		System.out.println("long   "+Long.MIN_VALUE+   " "+Long.MAX_VALUE);
+
+	}
+
+}
